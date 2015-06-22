0a642cd691496be10aba425904018235550e4642
Arun Perumal <arungda@gmail.com>
2015-06-22 22:46:42 +0530

    updated hard coded database usage

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
     "listenPort": 9080,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
     "securePort": 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
     "distFolder": path.resolve(__dirname, '../../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
-    "mongoHost" : 'arunperumal:navigator@ds53080.mongolab.com:53080/heroku_85bs2bx5',
-    "mongoPort"           : 53080,
+    "mongoHost" : 'arunperumal:navigator@ds53080.mongolab.com',
+    "mongoPort"           : 53080/heroku_85bs2bx5,
     "DB"                  : 'CloudDB',
     "roviCollection"      : "rovi",
     "tribuneCollection"   : "tribune"
