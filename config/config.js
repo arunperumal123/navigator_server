7fa2cb8d4a6d700786669b99b3344c1beffa3dc4
Arun Perumal <arungda@gmail.com>
2015-06-22 22:00:25 +0530

    updated the cloud mongo db URI

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
     "listenPort": 9080,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
     "securePort": 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
     "distFolder": path.resolve(__dirname, '../../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
-    "mongoHost" : 'localhost',
-    "mongoPort"           : 27017,
+    "mongoHost" : 'dqrgnbvgd7iif995l6el4t4dlf@ds053080.mongolab.com',
+    "mongoPort"           : 53080,
     "DB"                  : 'CloudDB',
     "roviCollection"      : "rovi",
     "tribuneCollection"   : "tribune"
