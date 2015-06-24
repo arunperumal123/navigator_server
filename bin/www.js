7e774fc86594f1e07e7d7c55cb02d572853abbbf
Arun Perumal <arungda@gmail.com>
2015-06-24 14:23:23 +0530

    don't commit to hard coded port. Instead , find out the port allocated and use it.

--- bin/www.js
+++ bin/www.js
@@ -3,6 +3,8 @@ var app = require('../app.js'),
 
 // START THE SERVER
 // =============================================================================
+var port = process.env.PORT || 9080;
+
 app.listen(config.server.listenPort);
 
-console.log('Magic happens on port ' + config.server.listenPort);
+console.log('Magic happens on port ' + port);
